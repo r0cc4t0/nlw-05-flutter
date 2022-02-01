0401fc873156f69a8a0dc4820afcba6656410cf2
r0cc4t0 <guilherme.roccato@outlook.com>
2022-01-31 23:07:13 -0300

    Score Card Widget

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,47 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:nlw_05_flutter/core/app_gradients.dart';
-import 'package:nlw_05_flutter/core/app_text_styles.dart';
+import 'package:nlw_05_flutter/core/core.dart';
+import 'package:nlw_05_flutter/home/widgets/scorecard/score_card_widget.dart';
 
 class AppBarWidget extends PreferredSize {
   AppBarWidget({Key? key})
       : super(
           key: key,
           preferredSize: const Size.fromHeight(250),
-          child: Container(
+          child: SizedBox(
             height: 250,
-            decoration: const BoxDecoration(
-              gradient: AppGradients.linear,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text.rich(
-                    TextSpan(
-                      text: 'Olá, ',
-                      style: AppTextStyles.title,
-                      children: [
-                        TextSpan(
-                          text: 'r0cc4t0!',
-                          style: AppTextStyles.titleBold,
-                        )
-                      ],
-                    ),
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  width: double.maxFinite,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: const BoxDecoration(
+                    gradient: AppGradients.linear,
                   ),
-                  Container(
-                    width: 58,
-                    height: 58,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: const DecorationImage(
-                        image: NetworkImage(
-                            'https://avatars.githubusercontent.com/u/60143866?v=4'),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                          text: 'Olá, ',
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                              text: 'r0cc4t0!',
+                              style: AppTextStyles.titleBold,
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                            image: NetworkImage(
+                                'https://avatars.githubusercontent.com/u/60143866?v=4'),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                )
+              ],
             ),
           ),
         );
---
+++ lib/home/widgets/chart/chart_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:nlw_05_flutter/core/core.dart';
+
+class ChartWidget extends StatelessWidget {
+  const ChartWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 80,
+      width: 80,
+      child: Stack(
+        children: [
+          const Center(
+            child: SizedBox(
+              height: 80,
+              width: 80,
+              child: CircularProgressIndicator(
+                strokeWidth: 10,
+                value: .75,
+                backgroundColor: AppColors.chartSecondary,
+                valueColor:
+                    AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              '75%',
+              style: AppTextStyles.heading,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
