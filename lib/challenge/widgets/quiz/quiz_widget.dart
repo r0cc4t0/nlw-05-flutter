02fdddea6433b33684d58a53281bbfbdee3f1738
r0cc4t0 <guilherme.roccato@outlook.com>
2022-02-24 21:27:44 -0300

    Quiz Controller

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 import '../../../core/core.dart';
 import '../answer/answer_widget.dart';
 import '../../../shared/models/question_model.dart';
+import '../../../shared/models/answer_model.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
 
   const QuizWidget({Key? key, required this.question}) : super(key: key);
+
+  @override
+  State<QuizWidget> createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int selectedIndex = -1;
+
+  AnswerModel answer(int index) => widget.question.answers[index];
 
   @override
   Widget build(BuildContext context) {
@@ -14,18 +24,19 @@ class QuizWidget extends StatelessWidget {
       children: [
         const SizedBox(height: 64),
         Text(
-          question.title,
+          widget.question.title,
           style: AppTextStyles.heading,
         ),
         const SizedBox(height: 24),
-        ...question.answers
-            .map(
-              (e) => AnswerWidget(
-                title: e.title,
-                isRight: e.isRight,
-              ),
-            )
-            .toList(),
+        for (var i = 0; i < widget.question.answers.length; i++)
+          AnswerWidget(
+            answer: answer(i),
+            isSelected: selectedIndex == i,
+            onTap: () {
+              selectedIndex = i;
+              setState(() {});
+            },
+          )
       ],
     );
   }
