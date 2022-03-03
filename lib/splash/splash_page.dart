ed0b20f01523e56701a20c1774d3489536464432
r0cc4t0 <guilherme.roccato@outlook.com>
2022-03-02 22:31:38 -0300

    Splash Screen Transition

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import '../core/core.dart';
+import '../home/home_page.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 2000)).then(
+      (_) => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      ),
+    );
+
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
